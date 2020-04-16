bce851bda8178b93fa2b458f36b121cc44b0dbe5
Egor Danchenkov <egor.danchenkov@rednuclearmonkey.com>
2020-04-16 21:11:58 +0300

    Update deploy process for menutube.rednuclearmonkey.com domain and some of metadata

--- gatsby-site/gatsby-config.js
+++ gatsby-site/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    pathPrefix: '/menutube',
+    pathPrefix: '/',
     siteMetadata: {
-        title: `Gatsby Default Starter`,
-        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+        title: `MenuTube`,
+        description: `Catch YouTube into your macOS menu bar!`,
         author: `@gatsbyjs`
     },
     plugins: [
