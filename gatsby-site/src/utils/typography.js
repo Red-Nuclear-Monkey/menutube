fc7dab56663ebb261c7d3f9324327358d14790bd
Egor Danchenkov <egor.danchenkov@rednuclearmonkey.com>
2020-04-16 19:12:20 +0300

    Add roboto

--- gatsby-site/src/utils/typography.js
+++ gatsby-site/src/utils/typography.js
@@ -2,8 +2,22 @@ import Typography from 'typography';
 
 const typography = new Typography({
     baseLineHeight: 1.2,
-    headerFontFamily: ['Roboto', 'Helvetica', 'sans-serif'],
-    bodyFontFamily: ['Roboto', 'Helvetica', 'sans-serif']
+    googleFonts: [
+        {
+            name: "Roboto",
+            styles: [100, 300, 400, 500, 700, 900],
+        }
+    ],
+    headerFontFamily: [
+        "Roboto",
+        "Helvetica",
+        "sans-serif",
+    ],
+    bodyFontFamily: [
+        "Roboto",
+        "Helvetica",
+        "sans-serif",
+    ]
 });
 
 export default typography;
